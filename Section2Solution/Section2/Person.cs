f14d24916b2b2eab1bff40bfd57de12d6b1a279f
kcrwilson <kcrwilson@gmail.com>
2015-09-22 20:22:24 -0400

    Update1

---
+++ Section2Solution/Section2/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace Section2
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+        public Person Spouse;
+        public bool IsMarried()
+        {
+            return Spouse != null;
+        }
+        static double AverageAge;
+
+        public string GetFullName()
+        {
+
+        }
+
+        void PrintNameAndAge()
+        {
+
+        }
+    }
+}
--- Section2Solution/Section2/Program.cs
+++ Section2Solution/Section2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
+            Person person1 = new Person();
+            System.Console.Write("Person 1 First Name: ");
+            person1.FirstName = System.Console.ReadLine();
+            System.Console.Write("Person 1 Last Name: ");
+            person1.LastName = System.Console.ReadLine();
+            System.Console.Write("Person 1 Age: ");
+            person1.Age = int.Parse(System.Console.ReadLine());
+            System.Console.Write("Person 1 Married? (y/n): ");
+            if (System.Console.ReadLine().ToLower().StartsWith("y"))
+            {
+                person1.Spouse = new Person();
+                System.Console.Write("Spouse's Name: ");
+                person1.Spouse.FirstName = System.Console.ReadLine();
+                System.Console.Write("Spouse's Age: ");
+                person1.Spouse.Age = int.Parse(System.Console.ReadLine());
+            }
+          
 
+            //Person person2 = new Person();
+            //person2.FirstName = "Temp2";
+            //person2.LastName = "Temp2.2";
+            //person2.age = 28;
+
+            //Person person3 = new Person();
+            //person3.FirstName = "Temp3";
+            //person3.LastName = "Temp3.2";
+            //person3.age = 38;
         }
     }
 }
