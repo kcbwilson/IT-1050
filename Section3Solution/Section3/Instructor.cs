1f191863496c7a5bc42ada8aaaaade0ff32cdd3f
Kevin Wilson <kcrwilson@gmail.com>
2015-10-04 15:02:36 -0400

    Solution3 Begin

---
+++ Section3Solution/Section3/Instructor.cs
@@ -0,0 +1,21 @@
+﻿namespace Section3
+{
+    class Instructor
+    {
+        private string Name;
+        //private string LastName;
+        private string CourseName;
+
+        public Instructor(string name, string courseName)
+        {
+            this.Name = name;
+            this.CourseName = courseName;
+        }
+
+        public void SetStudentGrade(Student name, Student grade)
+        {
+                        
+        }
+
+    }
+}
---
+++ Section3Solution/Section3/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace Section3
+{
+    class Student
+    {
+        private string Name;
+        private int Grade;
+        private Instructor Teacher;
+
+        public Student(string name, int grade)
+        {
+            this.Name = name;
+            this.Grade = grade;
+        }
+
+        public void SetGrade(int grade)
+        {
+            this.Grade = 0;
+        }
+    }
+}
