2c29bd18cf6efe8cec921b3730700b89357fea97
kcrwilson <kcrwilson@gmail.com>
2015-11-10 17:33:35 -0500

    11/10 Update

--- Section4Solution/Section4/Program.cs
+++ Section4Solution/Section4/Program.cs
@@ -4,9 +4,10 @@
     {
         static void Main(string[] args)
         {
-            int NumberOfChildren;
-            int NumberOfAdults;
-            int NumberOfSeniors;
+            int NumberOfChildren = 0;
+            int NumberOfAdults = 0;
+            int NumberOfSeniors = 0;
+            int TotalAttendies = 0;
 
             double TotalTicketPrice = 0;
 
@@ -23,6 +24,12 @@
             const double HotDog = 3.99;
             const double Popcorn = 4.50;
             const double Candy = 1.99;
+
+            double GrandTotal = 0;
+
+            int SodaCornDiscountQty = 0;
+            int FreePopcornQty = 0;
+            int CandyDiscountQty = 0;
 
             Ticket ChildMatinee = new Ticket("Child Matinee Ticket ", 3.99);
             Ticket AdultMatinee = new Ticket("Adult Matinee Ticket ", 5.99);
@@ -101,8 +108,55 @@
             TotalFoodPrice += (CandyQty * Candy);
 
             System.Console.WriteLine("Totals before discounts applied");
-            System.Console.WriteLine("Ticket Total: $" + TotalTicketPrice);
-            System.Console.WriteLine("Snack Total : $" + TotalFoodPrice);
+            System.Console.WriteLine("Ticket Price: $" + TotalTicketPrice);
+            System.Console.WriteLine("Snack Price : $" + TotalFoodPrice);
+            GrandTotal = TotalTicketPrice + TotalFoodPrice;
+            System.Console.WriteLine("Total       : $" + GrandTotal);
+
+            //Each purchase of a popcorn and a large soda grants a $2 discount on one movie ticket.
+
+            if (PopCornQty > 0 && LargeSodaQty > 0)
+            {
+                if (PopCornQty < LargeSodaQty)
+                {
+                    SodaCornDiscountQty = PopCornQty;
+                }
+
+                else if (LargeSodaQty > PopCornQty)
+                {
+                    SodaCornDiscountQty = LargeSodaQty;
+                }
+                else
+                {
+                    SodaCornDiscountQty = LargeSodaQty;
+                }
+            }
+
+            //If 3 or more people purchase tickets to an evening show, they get 1 free bag of popcorn.
+
+            TotalAttendies = NumberOfAdults + NumberOfChildren + NumberOfSeniors;
+
+            if (TotalAttendies >= 3)
+            {
+                FreePopcornQty = TotalAttendies / 3;
+            }
+
+            //For each 3 candies you buy, you can get a 4th candy for free.
+
+            if (CandyQty >= 4)
+            {
+                CandyDiscountQty = CandyQty / 4;
+            }
+
+            System.Console.WriteLine("Total Discounts Earned");
+            System.Console.WriteLine("$2.00 off Tickets earned     : " + SodaCornDiscountQty);
+            System.Console.WriteLine("Number of Free Popcorn earned: " + FreePopcornQty);
+            System.Console.WriteLine("Number of Free Candy earned  : " + CandyDiscountQty);
+
+            GrandTotal = GrandTotal - (SodaCornDiscountQty * 2) - (FreePopcornQty * Popcorn) - (CandyDiscountQty * Candy);
+
+            System.Console.WriteLine("Total After Discounts Applied");
+            System.Console.WriteLine("Total       : $" + GrandTotal);
 
             System.Console.ReadKey();
         }
